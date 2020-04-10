1801c31942d32ba10216222dadf3f7837dc62857
LIBIDORI <francisgarciaramos@hotmail.com>
2020-04-10 18:48:05 +0200

    Added files and changes required for deployment to heroku

--- LIBISite/settings.py
+++ LIBISite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','#(fnd%1%_xo6#6%1pwbku4mlcjy*48_
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
